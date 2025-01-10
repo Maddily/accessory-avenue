70523ab5a143146e5a2612a146545f389ae6fc07
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-10 09:49:03 +0200

    Add App component's initial structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import useScreenResize from './hooks/useScreenWidth';
+import Nav from './components/Nav/Nav';
+import Menu from './components/Menu/Menu';
+import './styles/normalize.css';
+import './styles/App.css';
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  /**
+   * If screen is resized to 500px in width or higher,
+   * menuOpen is set to false (only if it's set to open).
+   * This makes sure that if the screen is resized again
+   * below 500px, the menu is collapsed.
+   */
+  useScreenResize(menuOpen, setMenuOpen);
+
+  return (
+    <>
+      <Nav menuOpen={menuOpen} menuOpenSetter={(value) => setMenuOpen(value)} />
+      {menuOpen && <Menu />}
+    </>
+  );
+}
+
+export default App;
