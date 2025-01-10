8219a826f266ee1c246c89ed9487f978715881b7
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-10 09:18:09 +0200

    Create Nav component initial structure

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,64 @@
+import logo from '../../assets/images/logo.jpg';
+import styles from './Nav.module.css';
+import NavButton from '../NavButton/NavButton';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'lucide-react';
+import Icon from '@mdi/react';
+import { mdiMenu, mdiClose } from '@mdi/js';
+import PropTypes from 'prop-types';
+
+/**
+ * Renders a nav contaning the logo, nav links and cart button.
+ *
+ * @param {boolean} menuOpen - Indicates if the dropdown menu is open.
+ * @param {function(boolean)} menuOpenSetter - Sets menuOpen state.
+ * @returns {JSX.Element}
+ */
+export default function Nav({ menuOpen, menuOpenSetter }) {
+  return (
+    <nav className={styles.nav}>
+      <img className={styles.logo} src={logo} alt="logo" />
+      <ul className={styles['nav-buttons']}>
+        <li className={styles['nav-button-container']}>
+          <NavButton value="home" path="/" />
+        </li>
+        <li className={styles['nav-button-container']}>
+          <NavButton value="shop" path="/" />
+        </li>
+      </ul>
+      <div className={styles['cart-menu-close-container']}>
+        <Link to="/" className={styles.cart}>
+          <ShoppingCart />
+          <span className={styles['cart-number']}>0</span>
+        </Link>
+        {/* Render a menu or a close menu button */}
+        {menuOpen ? (
+          <Icon
+            path={mdiClose}
+            size={1}
+            title="Close Menu"
+            className={styles.x}
+            onClick={() => {
+              menuOpenSetter(false);
+            }}
+          />
+        ) : (
+          <Icon
+            path={mdiMenu}
+            size={1}
+            title="Menu"
+            className={styles['menu-icon']}
+            onClick={() => {
+              menuOpenSetter(true);
+            }}
+          />
+        )}
+      </div>
+    </nav>
+  );
+}
+
+Nav.propTypes = {
+  menuOpen: PropTypes.bool,
+  menuOpenSetter: PropTypes.func,
+};
