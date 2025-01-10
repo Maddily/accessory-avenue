db6678ed7f3dfe91edbff565b468be15920f8cee
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-10 09:40:01 +0200

    Add Menu component initial structure

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,22 @@
+import styles from './Menu.module.css';
+import MenuButton from '../MenuButton/MenuButton';
+
+/**
+ * Renders a menu of nav buttons.
+ *
+ * @returns {JSX.Element}
+ */
+export default function Menu() {
+  return (
+    <nav className={styles.menu}>
+      <ul className={styles['menu-buttons']}>
+        <li className={styles['menu-button-container']}>
+          <MenuButton value="home" path="/" />
+        </li>
+        <li className={styles['menu-button-container']}>
+          <MenuButton value="shop" path="/" />
+        </li>
+      </ul>
+    </nav>
+  );
+}
