214478b9d24ebd67e15cc6607cdd066bcc455ac7
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-13 14:51:42 +0200

    Use beforeEach hook to avoid repeating setup code

--- src/components/Cart/Cart.test.jsx
+++ src/components/Cart/Cart.test.jsx
@@ -1,20 +1,24 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import Cart from './Cart';
 
 describe('Cart', () => {
-  it('renders a link that redirects to the correct path', async () => {
-    const user = userEvent.setup();
+  let cartLink;
 
+  beforeEach(() => {
     render(
       <BrowserRouter>
         <Cart path="cart" />
       </BrowserRouter>
     );
 
-    const cartLink = screen.getByRole('link', { name: 'cart' });
+    cartLink = screen.getByRole('link', { name: 'cart' });
+  });
+
+  it('renders a link that redirects to the correct path', async () => {
+    const user = userEvent.setup();
 
     await user.click(cartLink);
 
@@ -22,13 +26,6 @@ describe('Cart', () => {
   });
 
   it('renders the number of items in the cart', () => {
-    render(
-      <BrowserRouter>
-        <Cart path="cart" />
-      </BrowserRouter>
-    );
-
-    const cartLink = screen.getByRole('link', { name: 'cart' });
     const numberOfItems = screen.getByRole('generic', {
       name: 'number of items in cart',
     });
