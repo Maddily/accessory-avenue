7cdf2956c154714a5429f1a165171d89e638564d
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-13 09:39:15 +0200

    Create Cart component

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,19 @@
+import { ShoppingCart } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import styles from './Cart.module.css';
+
+/**
+ * Renders a cart icon that links to the cart page
+ * and displays the number of items in the cart.
+ *
+ * @returns {JSX.Element}
+ */
+export default function Cart() {
+  return (
+    <Link to="/" className={styles.cart}>
+      <ShoppingCart />
+      {/* Refactor so the number is displayed only if higher than 0 */}
+      <span className={styles['cart-number']}>0</span>
+    </Link>
+  );
+}
