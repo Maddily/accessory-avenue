60f3c0db3b14030085dd4a111d6c3bf773a01223
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-13 14:55:14 +0200

    Use beforeEach hook to avoid repeating setup code

--- src/components/MenuButton/MenuButton.test.jsx
+++ src/components/MenuButton/MenuButton.test.jsx
@@ -1,27 +1,24 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 import MenuButton from './MenuButton';
 
 describe('MenuButton', () => {
-  it('renders a link with correct text', () => {
-    render(
-      <BrowserRouter>
-        <MenuButton value="shop" path="/" />
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('link').textContent).toMatch(/shop/i);
-  });
-
-  it('links to the correct page', async () => {
-    const user = userEvent.setup();
-
+  beforeEach(() => {
     render(
       <BrowserRouter>
         <MenuButton value="shop" path="/shop" />
       </BrowserRouter>
     );
+  });
+
+  it('renders a link with correct text', () => {
+    expect(screen.getByRole('link').textContent).toMatch(/shop/i);
+  });
+
+  it('links to the correct page', async () => {
+    const user = userEvent.setup();
 
     const shopButton = screen.getByRole('link', {
       name: /shop/i,
