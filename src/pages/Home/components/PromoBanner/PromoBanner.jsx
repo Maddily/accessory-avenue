0b53b052ad739c737004c452e51f1a2473bab56b
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-15 10:09:17 +0200

    Create PromoBanner component

---
+++ src/pages/Home/components/PromoBanner/PromoBanner.jsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import styles from './PromoBanner.module.css';
+
+/**
+ * Renders a promotional banner.
+ *
+ * @returns {JSX.Element}
+ */
+export default function PromoBanner() {
+  return (
+    <article className={styles.promo}>
+      <h2 className={styles['promo-heading']}>
+        Free Shipping on Orders Over $50!
+      </h2>
+      <Link to="/" className={styles.cta} type="button">
+        <span className={styles.underline}>shop now</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="10"
+          viewBox="0 0 46 16"
+        >
+          <path
+            id="Path_10"
+            data-name="Path 10"
+            d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z"
+            transform="translate(30)"
+          ></path>
+        </svg>
+      </Link>
+    </article>
+  );
+}
