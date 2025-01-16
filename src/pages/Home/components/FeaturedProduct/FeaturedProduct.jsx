75acaa7a1b139a90e66630b59b9d8d0b595a95aa
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-16 11:34:53 +0200

    Create FeaturedProduct component

---
+++ src/pages/Home/components/FeaturedProduct/FeaturedProduct.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import styles from './FeaturedProduct.module.css';
+import Stars from '../../../../components/Stars/Stars';
+import AddToCart from '../../../../components/AddToCart/AddToCart';
+
+/**
+ * Renders a product.
+ *
+ * @param {object} product - The data of a product
+ * @returns {JSX.Element}
+ */
+export default function FeaturedProduct({ product }) {
+  return (
+    <article tabIndex={0} aria-label='product' className={styles.product}>
+      <img className={styles.image} src={product.image} alt={product.title} />
+      <h3 className={styles['product-title']}>{product.title}</h3>
+      <div className={styles.rating}>
+        <span aria-label='star rating'>
+          <Stars rating={product.rating} title={product.title} />
+        </span>
+        <span aria-label='rating'>{product.rating}</span>
+      </div>
+      <div className={styles['card-footer']}>
+        <p aria-label='price' className={styles.price}>${product.price}</p>
+        <AddToCart />
+      </div>
+    </article>
+  );
+}
+
+FeaturedProduct.propTypes = {
+  product: PropTypes.object,
+};
