fcd72eb6a75286be61c8a3a58ac9e470092b0c8f
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-16 10:33:30 +0200

    Replace div container with button
    The button makes more sense because clicking
    will cause an action, and it adds keyboard
    navigation support.

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -7,7 +7,7 @@ import styles from './AddToCart.module.css';
  */
 export default function AddToCart() {
   return (
-    <div className={styles.cart}>
+    <button type='button' className={styles.cart}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -25,6 +25,6 @@ export default function AddToCart() {
         <path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"></path>
         Add to cart
       </svg>
-    </div>
+    </button>
   );
 }
