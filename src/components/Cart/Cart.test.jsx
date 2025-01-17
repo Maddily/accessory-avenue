a6885c2815bad9c793faa529b79d996eacc43a2d
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-17 09:37:01 +0200

    Test the shopping cart icon exists

--- src/components/Cart/Cart.test.jsx
+++ src/components/Cart/Cart.test.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Cart from './Cart';
 
 vi.mock('lucide-react', () => ({
-  ShoppingCart: vi.fn(() => <svg data-testid='shopping-cart-icon'></svg>)
+  ShoppingCart: vi.fn(() => <svg data-testid="shopping-cart-icon"></svg>),
 }));
 
 describe('Cart', () => {
@@ -22,6 +22,12 @@ describe('Cart', () => {
 
   it('renders the cart link with the correct path', async () => {
     expect(cartLink).toHaveAttribute('href', '/cart');
+  });
+
+  it('renders the ShoppingCart icon', () => {
+    const shoppingCartIcon = screen.getByTestId('shopping-cart-icon');
+
+    expect(shoppingCartIcon).toBeInTheDocument();
   });
 
   it('renders the number of items in the cart', () => {
