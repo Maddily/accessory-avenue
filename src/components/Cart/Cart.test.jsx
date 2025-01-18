6bb8d69817333e61776350f312e9c8dae549d939
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-18 17:14:53 +0200

    Unmock cart icon

--- src/components/Cart/Cart.test.jsx
+++ src/components/Cart/Cart.test.jsx
@@ -1,11 +1,7 @@
-import { vi, describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Cart from './Cart';
-
-vi.mock('lucide-react', () => ({
-  ShoppingCart: vi.fn(() => <svg data-testid="shopping-cart-icon"></svg>),
-}));
 
 describe('Cart', () => {
   let cartLink;
@@ -22,12 +18,6 @@ describe('Cart', () => {
 
   it('renders the cart link with the correct path', async () => {
     expect(cartLink).toHaveAttribute('href', '/cart');
-  });
-
-  it('renders the ShoppingCart icon', () => {
-    const shoppingCartIcon = screen.getByTestId('shopping-cart-icon');
-
-    expect(shoppingCartIcon).toBeInTheDocument();
   });
 
   it('renders the number of items in the cart', () => {
