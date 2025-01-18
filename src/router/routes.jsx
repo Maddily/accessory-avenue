221d42f5ce770166bf81f5718a05981706f1de1c
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-18 20:29:56 +0200

    Add children routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,23 @@
 import App from '../App';
+import HomeContent from '../pages/Home/components/HomeContent/HomeContent';
+import ShopContent from '../pages/Shop/components/ShopContent/ShopContent';
+import { productsLoader } from './loaders';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <HomeContent />,
+      },
+      {
+        path: '/shop',
+        loader: productsLoader,
+        element: <ShopContent />,
+      },
+    ],
   },
 ];
 
