42ec7f18e56fc811ddaaa440a879cbbbf9ffb84a
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-21 09:51:33 +0200

    Update mock

--- src/pages/Home/components/HomeContent/HomeContent.test.jsx
+++ src/pages/Home/components/HomeContent/HomeContent.test.jsx
@@ -1,6 +1,6 @@
 import { vi, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { HomeContent } from './HomeContent';
+import HomeContent from './HomeContent';
 import Hero from '../Hero/Hero';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
 import Testimonials from '../Testimonials/Testimonials';
@@ -11,20 +11,20 @@ vi.mock('../FeaturedProducts/FeaturedProducts');
 vi.mock('../Testimonials/Testimonials');
 vi.mock('../PromoBanner/PromoBanner');
 
-Hero.mockImplementation(() => <div>Hero Component</div>);
+Hero.mockImplementation(() => <div data-testid="hero"></div>);
 FeaturedProducts.mockImplementation(() => (
-  <div>Featured Products Component</div>
+  <div data-testid="featured-products"></div>
 ));
-Testimonials.mockImplementation(() => <div>Testimonials Component</div>);
-PromoBanner.mockImplementation(() => <div>Promo Banner Component</div>);
+Testimonials.mockImplementation(() => <div data-testid="testimonials"></div>);
+PromoBanner.mockImplementation(() => <div data-testid="promo-banner"></div>);
 
 describe('HomeContent', () => {
   it('renders all child components ', () => {
     render(<HomeContent />);
 
-    expect(screen.getByText('Hero Component')).toBeInTheDocument();
-    expect(screen.getByText('Featured Products Component')).toBeInTheDocument();
-    expect(screen.getByText('Testimonials Component')).toBeInTheDocument();
-    expect(screen.getByText('Promo Banner Component')).toBeInTheDocument();
+    expect(screen.getByTestId('hero')).toBeInTheDocument();
+    expect(screen.getByTestId('featured-products')).toBeInTheDocument();
+    expect(screen.getByTestId('testimonials')).toBeInTheDocument();
+    expect(screen.getByTestId('promo-banner')).toBeInTheDocument();
   });
 });
