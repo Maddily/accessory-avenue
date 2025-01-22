039314cd4cca75d4fcc906bf7a0078cd34302f0d
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-22 11:55:41 +0200

    Receive and display the number of products in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,17 +8,23 @@ import styles from './Cart.module.css';
  * Renders a cart icon that links to the cart page
  * and displays the number of items in the cart.
  *
+ * @param {array} productsInCart - The product objects added to cart.
  * @returns {JSX.Element}
  */
-export default function Cart({ path }) {
+export default function Cart({ productsInCart }) {
   return (
-    <Link aria-label="cart" to={path} className={styles.cart}>
-      <Icon title='Go to cart' color='#fff' path={mdiCart} size={1.2} />
-      <span aria-label='number of items in cart' className={styles['cart-number']}>(0)</span>
+    <Link aria-label="cart" to='/' className={styles.cart}>
+      <Icon title="Go to cart" color="#fff" path={mdiCart} size={1.2} />
+      <span
+        aria-label="number of items in cart"
+        className={styles['cart-number']}
+      >
+        ({productsInCart.length})
+      </span>
     </Link>
   );
 }
 
 Cart.propTypes = {
-  path: PropTypes.string,
+  productsInCart: PropTypes.array,
 };
