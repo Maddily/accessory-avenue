0f08953ee0fa00552f232d79889101827f1281df
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-28 14:53:31 +0200

    Mock useLoading hook and loading component

--- src/pages/FAQS/components/FaqsContent/FaqsContent.test.jsx
+++ src/pages/FAQS/components/FaqsContent/FaqsContent.test.jsx
@@ -2,6 +2,16 @@ import { vi, describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import FaqsContent from './FaqsContent';
 import FaqsGroup from '../FaqsGroup/FaqsGroup';
+import useLoading from '../../../../hooks/useLoading';
+
+vi.mock('../../../../hooks/useLoading', () => ({
+  __esModule: true,
+  default: vi.fn(),
+}));
+
+vi.mock('react-loader-spinner', () => ({
+  ThreeDots: vi.fn(() => <div data-testid="loading-animation"></div>),
+}));
 
 vi.mock('../FaqsGroup/FaqsGroup');
 FaqsGroup.mockImplementation(() => <div data-testId="group"></div>);
@@ -35,7 +45,27 @@ describe('FaqsContent', () => {
   ];
 
   beforeEach(() => {
+    vi.mocked(useLoading).mockReturnValue({
+      loading: false,
+    });
+
     render(<FaqsContent faqGroups={faqGroups} />);
+  });
+
+  it('renders loading component', () => {
+    vi.mocked(useLoading).mockReturnValueOnce({
+      loading: true,
+    });
+
+    render(<FaqsContent />);
+
+    const loadingComponent = screen.getByTestId('loading-animation');
+    expect(loadingComponent).toBeInTheDocument();
+  });
+
+  it('does not render loading component when loading is false', () => {
+    const loadingComponent = screen.queryByTestId('loading-animation');
+    expect(loadingComponent).toBeNull();
   });
 
   it('renders FAQS heading', () => {
