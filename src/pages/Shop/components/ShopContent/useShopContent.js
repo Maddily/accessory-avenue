3ec0374bd64905771c6e7e75c15757588fc5df69
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-28 14:55:36 +0200

    Move loading state management to useLoading hook

--- src/pages/Shop/components/ShopContent/useShopContent.js
+++ src/pages/Shop/components/ShopContent/useShopContent.js
@@ -1,22 +1,12 @@
-import { useState, useEffect } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 /**
- * A custom hook responsible for managing loading state
- * and retrieving products data from the loader.
+ * A custom hook for retrieving products data from the loader.
  *
- * @returns loading state value and fetched products.
+ * @returns the fetched products.
  */
 export default function useShopContent() {
-  const [loading, setLoading] = useState(true);
   const { products } = useLoaderData();
 
-  // Set loading state to false one second after the component is mounted
-  useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 1000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  return { loading, products };
+  return { products };
 }
