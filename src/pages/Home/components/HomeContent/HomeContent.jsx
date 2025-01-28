5cec131374bd1f6c8049b014a050fb6867484649
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-28 15:03:59 +0200

    Use a loading animation before displaying data

--- src/pages/Home/components/HomeContent/HomeContent.jsx
+++ src/pages/Home/components/HomeContent/HomeContent.jsx
@@ -2,6 +2,8 @@ import Hero from '../Hero/Hero';
 import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
 import Testimonials from '../Testimonials/Testimonials';
 import PromoBanner from '../PromoBanner/PromoBanner';
+import useLoading from '../../../../hooks/useLoading';
+import { ThreeDots } from 'react-loader-spinner';
 
 /**
  * Renders components associated with home page.
@@ -9,6 +11,22 @@ import PromoBanner from '../PromoBanner/PromoBanner';
  * @returns {JSX.Element}
  */
 export default function HomeContent() {
+  const { loading } = useLoading();
+
+  if (loading)
+    return (
+      <ThreeDots
+        visible={true}
+        height="80"
+        width="80"
+        color="#5a9592"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass="loader"
+      />
+    );
+
   return (
     <>
       <Hero />
