b8d5166f5a68505a08f47ebfa88159c475034803
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-31 14:15:25 +0200

    Display a loading animation

--- src/pages/Cart/components/CartContent/CartContent.jsx
+++ src/pages/Cart/components/CartContent/CartContent.jsx
@@ -1,9 +1,26 @@
 import { useOutletContext } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
+import useLoading from '../../../../hooks/useLoading';
+import { ThreeDots } from 'react-loader-spinner';
 import styles from './CartContent.module.css';
 
 export default function CartContent() {
+  const { loading } = useLoading();
   const [productsInCart] = useOutletContext();
+
+  if (loading)
+    return (
+      <ThreeDots
+        visible={true}
+        height="80"
+        width="80"
+        color="#5a9592"
+        radius="9"
+        ariaLabel="three-dots-loading"
+        wrapperStyle={{}}
+        wrapperClass="loader"
+      />
+    );
 
   return (
     <section className={styles.cart} aria-label="cart">
@@ -12,7 +29,9 @@ export default function CartContent() {
         <caption className={styles['visually-hidden']}>your cart</caption>
         <thead>
           <tr>
-            <th scope="col" colSpan='2'>product</th>
+            <th scope="col" colSpan="2">
+              product
+            </th>
             <th scope="col" className={styles.total1}>
               total
             </th>
