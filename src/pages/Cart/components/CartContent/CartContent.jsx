9546b8569948958f09233d59217aaf7eee4b4a10
Maddie <115709272+Maddily@users.noreply.github.com>
2025-01-31 14:27:56 +0200

    Show a message when the cart is empty

--- src/pages/Cart/components/CartContent/CartContent.jsx
+++ src/pages/Cart/components/CartContent/CartContent.jsx
@@ -2,6 +2,7 @@ import { useOutletContext } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
 import useLoading from '../../../../hooks/useLoading';
 import { ThreeDots } from 'react-loader-spinner';
+import { Link } from 'react-router-dom';
 import styles from './CartContent.module.css';
 
 export default function CartContent() {
@@ -24,31 +25,45 @@ export default function CartContent() {
 
   return (
     <section className={styles.cart} aria-label="cart">
-      <h1 className={styles.heading}>your cart</h1>
-      <table className={styles['cart-items']}>
-        <caption className={styles['visually-hidden']}>your cart</caption>
-        <thead>
-          <tr>
-            <th scope="col" colSpan="2">
-              product
-            </th>
-            <th scope="col" className={styles.total1}>
-              total
-            </th>
-            <th scope="col" className={styles['quantity-heading']}>
-              quantity
-            </th>
-            <th scope="col" className={styles.total2}>
-              total
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {productsInCart.map((productInCart) => (
-            <CartItem key={productInCart.id} productInCart={productInCart} />
-          ))}
-        </tbody>
-      </table>
+      {productsInCart.length === 0 ? (
+        <div className={styles['empty-cart']}>
+          <h2 className={styles['empty-cart-heading']}>Your cart is empty</h2>
+          <Link className={styles['home-link']} to="/">
+            Back to Homepage
+          </Link>
+        </div>
+      ) : (
+        <>
+          <h1 className={styles.heading}>your cart</h1>
+          <table className={styles['cart-items']}>
+            <caption className={styles['visually-hidden']}>your cart</caption>
+            <thead>
+              <tr>
+                <th scope="col" colSpan="2">
+                  product
+                </th>
+                <th scope="col" className={styles.total1}>
+                  total
+                </th>
+                <th scope="col" className={styles['quantity-heading']}>
+                  quantity
+                </th>
+                <th scope="col" className={styles.total2}>
+                  total
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {productsInCart.map((productInCart) => (
+                <CartItem
+                  key={productInCart.id}
+                  productInCart={productInCart}
+                />
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
     </section>
   );
 }
