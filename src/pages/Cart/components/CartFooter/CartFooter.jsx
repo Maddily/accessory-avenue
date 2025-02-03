df8de567522530d67500f3c10f6bc91740345ffc
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-03 13:11:42 +0200

    Create CartFooter component

---
+++ src/pages/Cart/components/CartFooter/CartFooter.jsx
@@ -0,0 +1,31 @@
+import styles from './CartFooter.module.css';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+/**
+ * Renders the estimated total and check out button.
+ *
+ * @param {number} total - The total price of all items added to the cart.
+ * @returns {JSX.Element}
+ */
+export default function CartFooter({ total }) {
+  return (
+    <div className={styles['cart-footer']}>
+      <p className={styles['estimated-total']} aria-label="estimated total">
+        Estimated total:{' '}
+        <span className={styles.total}>${total.toFixed(2)}</span>
+      </p>
+      <p
+        className={styles.note}
+        aria-label="taxes, disacount and shipping note"
+      >
+        Taxes, Discounts and shipping calculated at checkout
+      </p>
+      <Link className={styles.checkout} to="#">Check out</Link>
+    </div>
+  );
+}
+
+CartFooter.propTypes = {
+  total: PropTypes.number,
+};
