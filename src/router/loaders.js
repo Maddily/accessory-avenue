b5feb4ea052ca30e77d14fc6a54a1cb47b2effd0
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-04 11:05:53 +0200

    Document loader function

--- src/router/loaders.js
+++ src/router/loaders.js
@@ -1,3 +1,6 @@
+/**
+ * Fetches products data from the DummyJSON API.
+ */
 export async function productsLoader() {
   return fetch(
     'https://dummyjson.com/products/category/mobile-accessories'
