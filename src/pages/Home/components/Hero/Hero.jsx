2e52a1e22b096e4f2155f9903667b357ed5179ae
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-06 13:12:34 +0200

    Use hero images of different sizes and types
    The different-sized images are used in different
    screen widths and the png images are
    used as a fallback if webp is not supported
    by the user's browser.

--- src/pages/Home/components/Hero/Hero.jsx
+++ src/pages/Home/components/Hero/Hero.jsx
@@ -1,5 +1,12 @@
 import { Link } from 'react-router-dom';
-import heroImage from '../../../../assets/images/hero.png';
+import heroImage500Png from '../../../../assets/images/hero-500.png';
+import heroImage600Png from '../../../../assets/images/hero-600.png';
+import heroImage800Png from '../../../../assets/images/hero-800.png';
+import heroImage1000Png from '../../../../assets/images/hero-1000.png';
+import heroImage500 from '../../../../assets/images/hero-500.webp';
+import heroImage600 from '../../../../assets/images/hero-600.webp';
+import heroImage800 from '../../../../assets/images/hero-800.webp';
+import heroImage1000 from '../../../../assets/images/hero-1000.webp';
 import styles from './Hero.module.css';
 
 /**
@@ -9,22 +16,33 @@ import styles from './Hero.module.css';
  */
 export default function Hero() {
   return (
-    <section aria-label='hero' className={styles['hero-container']}>
+    <section aria-label="hero" className={styles['hero-container']}>
       <article className={styles['hero-text']}>
         <h1 className={styles['hero-heading']}>upgrade your everyday tech</h1>
-        <p aria-label='hero paragraph'>
+        <p aria-label="hero paragraph">
           Premium accessories designed to complement your devices with style and
           functionality.
         </p>
-        <Link to='/shop' className={styles.cta} type="button">
+        <Link to="/shop" className={styles.cta} type="button">
           shop now
         </Link>
       </article>
-      <img
-        className={styles['hero-image']}
-        src={heroImage}
-        alt="phone with accessories"
-      />
+      <picture>
+        <source srcSet={heroImage1000} type="image/webp"  media="(min-width: 1000px)" />
+        <source srcSet={heroImage800} type="image/webp"  media="(min-width: 800px)" />
+        <source srcSet={heroImage600} type="image/webp"  media="(min-width: 600px)" />
+        <source srcSet={heroImage500} type="image/webp" />
+
+        {/* A fallback for browsers that don't support WebP format */}
+        <source srcSet={heroImage1000Png} media="(min-width: 1000px)" />
+        <source srcSet={heroImage800Png} media="(min-width: 800px)" />
+        <source srcSet={heroImage600Png} media="(min-width: 600px)" />
+        <img
+          className={styles['hero-image']}
+          src={heroImage500Png}
+          alt="phone with accessories"
+        />
+      </picture>
     </section>
   );
 }
