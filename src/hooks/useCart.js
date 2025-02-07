92ecb7d88f46523fb4b075002bc236dc575a9791
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-07 18:13:24 +0200

    Implement local storage
    Products added to the cart persist when the page
    reloads and even when the browser is closed
    and reopened.

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 /**
  * Manages products in cart.
@@ -8,8 +8,16 @@ import { useState } from 'react';
 export default function useCart() {
   const [productsInCart, setProductsInCart] = useState([]);
 
+  // On component mount, retrieve data from local storage
+  useEffect(() => {
+    const storedProductsInCart = localStorage.getItem('productsInCart');
+
+    storedProductsInCart && setProductsInCart(JSON.parse(storedProductsInCart));
+  }, []);
+
   const updateProductsInCart = function (products) {
     setProductsInCart(products);
+    localStorage.setItem('productsInCart', JSON.stringify(products));
   };
 
   return { productsInCart, updateProductsInCart };
