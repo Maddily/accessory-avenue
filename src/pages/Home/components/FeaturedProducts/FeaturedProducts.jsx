7a6371140837d81e143689c6d264128e21dde0cb
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-10 12:14:34 +0200

    Import local featured product images

--- src/pages/Home/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/pages/Home/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,5 +1,9 @@
 import FeaturedProduct from '../FeaturedProduct/FeaturedProduct';
 import styles from './FeaturedProducts.module.css';
+import appleAirpodsImage from '../../../../assets/images/apple-airpods.png';
+import appleAirpowerWirelessChargerImage from '../../../../assets/images/apple-airpower-wireless-charger.png';
+import beatsFlexWirelessEarphonesImage from '../../../../assets/images/beats-flex-wireless-earphones.png';
+import tvStudioCameraPedestalImage from '../../../../assets/images/tv-studio-camera-pedestal.png';
 
 const products = [
   {
@@ -7,32 +11,28 @@ const products = [
     title: 'Apple Airpods',
     price: 129.99,
     rating: 4.38,
-    imageUrl:
-      'https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpods/thumbnail.png',
+    imageUrl: appleAirpodsImage,
   },
   {
     id: 102,
     title: 'Apple Airpower Wireless Charger',
     price: 79.99,
     rating: 4.51,
-    imageUrl:
-      'https://cdn.dummyjson.com/products/images/mobile-accessories/Apple%20Airpower%20Wireless%20Charger/thumbnail.png',
+    imageUrl: appleAirpowerWirelessChargerImage,
   },
   {
     id: 107,
     title: 'Beats Flex Wireless Earphones',
     price: 49.99,
     rating: 4.17,
-    imageUrl:
-      'https://cdn.dummyjson.com/products/images/mobile-accessories/Beats%20Flex%20Wireless%20Earphones/thumbnail.png',
+    imageUrl: beatsFlexWirelessEarphonesImage,
   },
   {
     id: 112,
     title: 'TV Studio Camera Pedestal',
     price: 499.99,
     rating: 4.05,
-    imageUrl:
-      'https://cdn.dummyjson.com/products/images/mobile-accessories/TV%20Studio%20Camera%20Pedestal/thumbnail.png',
+    imageUrl: tvStudioCameraPedestalImage,
   },
 ];
 
