a77646e3267f753d5c4252a914d6526e1a48c63e
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-27 09:45:05 +0200

    Move Cart component outside of Nav component
    Cart component uses a value calculated from
    a context. When the context updated, it resulted
    in Nav component re-rending, not just Cart
    component inside of it. Taking Cart component
    outside of Nav prevents that.

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,6 @@ import Icon from '@mdi/react';
 import { mdiMenu, mdiClose } from '@mdi/js';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import Cart from '../Cart/Cart';
 import useNav from './useNav';
 
 /**
@@ -15,12 +14,12 @@ import useNav from './useNav';
  * @param {function(boolean)} setMenuOpen - Sets menuOpen state.
  * @returns {JSX.Element}
  */
-export default function Nav({ menuOpen, setMenuOpen, productsInCart }) {
+export default function Nav({ menuOpen, setMenuOpen }) {
   useNav(setMenuOpen);
 
   return (
     <nav aria-label="header navigation" className={styles.nav}>
-      <Link aria-label='Go to home page' to='/' className={styles['logo-link']}>
+      <Link aria-label="Go to home page" to="/" className={styles['logo-link']}>
         <img className={styles.logo} src={logo} alt="logo" />
       </Link>
       <ul className={styles['nav-buttons']}>
@@ -34,41 +33,38 @@ export default function Nav({ menuOpen, setMenuOpen, productsInCart }) {
           <NavButton value="faqs" path="/faqs" />
         </li>
       </ul>
-      <div className={styles['cart-menu-close-container']}>
-        <Cart noOfProductsInCart={productsInCart.length} />
-        {/* Render a menu or a close menu button */}
-        {menuOpen ? (
-          <button
-            className={styles.x}
-            onClick={() => {
-              setMenuOpen(false);
-            }}
-          >
-            <Icon
-              color="#fff"
-              tabIndex={0}
-              path={mdiClose}
-              size={1}
-              title="Close menu"
-            />
-          </button>
-        ) : (
-          <button
-            className={styles['menu-icon']}
-            onClick={() => {
-              setMenuOpen(true);
-            }}
-          >
-            <Icon
-              color="#fff"
-              tabIndex={0}
-              path={mdiMenu}
-              size={1}
-              title="Menu"
-            />
-          </button>
-        )}
-      </div>
+      {/* Render a menu or a close menu button */}
+      {menuOpen ? (
+        <button
+          className={styles.x}
+          onClick={() => {
+            setMenuOpen(false);
+          }}
+        >
+          <Icon
+            color="#fff"
+            tabIndex={0}
+            path={mdiClose}
+            size={1}
+            title="Close menu"
+          />
+        </button>
+      ) : (
+        <button
+          className={styles['menu-icon']}
+          onClick={() => {
+            setMenuOpen(true);
+          }}
+        >
+          <Icon
+            color="#fff"
+            tabIndex={0}
+            path={mdiMenu}
+            size={1}
+            title="Menu"
+          />
+        </button>
+      )}
     </nav>
   );
 }
@@ -76,5 +72,5 @@ export default function Nav({ menuOpen, setMenuOpen, productsInCart }) {
 Nav.propTypes = {
   menuOpen: PropTypes.bool,
   setMenuOpen: PropTypes.func,
-  productsInCart: PropTypes.array,
 };
+
