4faca22a4768fe0fcd2a4c75659f0f1dae315935
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-27 18:33:33 +0200

    Do not retrieve data from the context
    Components that needed that data are now
    retrieving it themselves.

--- src/pages/Cart/components/CartItem/CartItem.jsx
+++ src/pages/Cart/components/CartItem/CartItem.jsx
@@ -1,5 +1,4 @@
 import ProductQuantity from '../ProductQuantity/ProductQuantity';
-import { useOutletContext } from 'react-router-dom';
 import useProduct from '../../../../hooks/useProduct';
 import styles from './CartItem.module.css';
 import PropTypes from 'prop-types';
@@ -11,12 +10,9 @@ import PropTypes from 'prop-types';
  * @returns {JSX.Element}
  */
 export default function CartItem({ productInCart }) {
-  const [productsInCart, dispatchCartAction] = useOutletContext();
   const currentQuantity = productInCart.quantity;
   const { quantity, updateQuantity } = useProduct({
     ...productInCart,
-    productsInCart,
-    dispatchCartAction,
     currentQuantity,
   });
 
@@ -30,9 +26,11 @@ export default function CartItem({ productInCart }) {
       </td>
       <td className={styles['product-details']}>
         <h2 className={styles['product-title']}>{productInCart.title}</h2>
-        <p aria-label='product price' className={styles['product-price']}>${productInCart.price}</p>
+        <p aria-label="product price" className={styles['product-price']}>
+          ${productInCart.price}
+        </p>
       </td>
-      <td aria-label='total price' className={styles['total1-data']}>
+      <td aria-label="total price" className={styles['total1-data']}>
         ${(productInCart.quantity * productInCart.price).toFixed(2)}
       </td>
       <td className={styles['quantity-data']}>
@@ -40,11 +38,10 @@ export default function CartItem({ productInCart }) {
           id={productInCart.id}
           title={productInCart.title}
           updateQuantity={updateQuantity}
-          dispatchCartAction={dispatchCartAction}
           quantity={quantity}
         />
       </td>
-      <td aria-label='total price' className={styles['total2-data']}>
+      <td aria-label="total price" className={styles['total2-data']}>
         ${(productInCart.quantity * productInCart.price).toFixed(2)}
       </td>
     </tr>
