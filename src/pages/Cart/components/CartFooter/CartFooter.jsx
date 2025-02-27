c040ba5c60b7030e53c80e63eb9ffd55a38bff89
Maddie <115709272+Maddily@users.noreply.github.com>
2025-02-27 10:15:43 +0200

    Receive the total amount as a string
    `toFixed` is being used on the total amount
    before passing it to CartFooter component.

--- src/pages/Cart/components/CartFooter/CartFooter.jsx
+++ src/pages/Cart/components/CartFooter/CartFooter.jsx
@@ -5,15 +5,14 @@ import { Link } from 'react-router-dom';
 /**
  * Renders the estimated total and check out button.
  *
- * @param {number} total - The total price of all items added to the cart.
+ * @param {string} total - The total price of all items added to the cart.
  * @returns {JSX.Element}
  */
 export default function CartFooter({ total }) {
   return (
     <div className={styles['cart-footer']}>
       <p className={styles['estimated-total']} aria-label="estimated total">
-        Estimated total:{' '}
-        <span className={styles.total}>${total.toFixed(2)}</span>
+        Estimated total: <span className={styles.total}>${total}</span>
       </p>
       <p
         className={styles.note}
@@ -21,11 +20,13 @@ export default function CartFooter({ total }) {
       >
         Taxes, Discounts and shipping calculated at checkout
       </p>
-      <Link className={styles.checkout} to="#">Check out</Link>
+      <Link className={styles.checkout} to="#">
+        Check out
+      </Link>
     </div>
   );
 }
 
 CartFooter.propTypes = {
-  total: PropTypes.number,
+  total: PropTypes.string,
 };
