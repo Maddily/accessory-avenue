714b69f6d3a2e8537e6a568f1cec6e72b637c9bb
Maddie <115709272+Maddily@users.noreply.github.com>
2025-03-03 11:22:37 +0200

    Mock useContext and update the test

--- src/pages/Cart/components/ProductQuantity/useProductQuantity.test.js
+++ src/pages/Cart/components/ProductQuantity/useProductQuantity.test.js
@@ -2,23 +2,30 @@ import { act, renderHook } from '@testing-library/react';
 import { it, vi, describe, expect } from 'vitest';
 import useProductQuantity from './useProductQuantity';
 
+const dispatchCartActionMock = vi.fn();
+
+vi.mock('react', async (importOriginal) => {
+  const actualReact = await importOriginal();
+  return {
+    ...actualReact,
+    useContext: () => dispatchCartActionMock,
+  };
+});
+
 describe('useProductQuantity', () => {
   let result;
 
   it('returns a function that calls dispatchCartAction with remove_from_cart action type and product id', () => {
-    const dispatchCartAction = vi.fn();
     const productId = 1;
 
-    ({ result } = renderHook(() =>
-      useProductQuantity(productId, dispatchCartAction)
-    ));
+    ({ result } = renderHook(() => useProductQuantity(productId)));
 
     const deleteProduct = result.current.deleteProduct;
     act(() => deleteProduct());
 
-    expect(dispatchCartAction).toHaveBeenCalledWith({
+    expect(dispatchCartActionMock).toHaveBeenCalledWith({
       type: 'remove_from_cart',
       productId,
     });
   });
-});
+}); 
